02407b41b617474f3deca6d41b97ca88788553e5
Marcelo Viana <marcelofv12@hotmail.com>
2023-11-22 17:24:55 -0300

    Correcao dos testes unit

--- test/src/modules/details/domain/usecases/_is_auth_required_test.dart
+++ test/src/modules/details/domain/usecases/_is_auth_required_test.dart
@@ -3,7 +3,7 @@ import 'package:gallery/widget.dart';
 import 'package:gallery/modules.dart';
 import 'package:gallery/src/modules/auth/auth_module.dart';
 import 'package:gallery/src/modules/details/domain/entities/user_info.dart';
-import 'package:gallery/src/modules/details/domain/usecases/on_get_contact_pressed.dart';
+import 'package:gallery/src/modules/details/domain/usecases/on_contact_canil_pressed.dart';
 
 import '../../../fakes/auth_module.dart';
 
