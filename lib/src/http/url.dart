e222afcf7cae424d261380e765b81e62f86d2c6a
Marcelo Viana <marcelofv12@hotmail.com>
2023-12-05 22:34:21 -0300

    fix: uri point to running server

--- lib/src/http/url.dart
+++ lib/src/http/url.dart
@@ -1,4 +1,11 @@
-import 'dart:io';
+import 'package:flutter/foundation.dart';
 
+String get uriPreffix => kReleaseMode
+    ? ''
+    : kProfileMode
+        ? 'stg.'
+        : kDebugMode
+            ? 'dev.'
+            : '';
 
-final baseUrl = Platform.isAndroid  ?"http://10.0.2.2:8000" : "http://127.0.0.1:8000";
+String get baseUrl => "${uriPreffix}api.dreampuppy.com.br";
