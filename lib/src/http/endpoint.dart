78f873084f6349f1809cfb4493cf761724877045
Marcelo Viana <marcelofv12@hotmail.com>
2024-02-28 15:46:25 -0300

    Update endpoint.dart

--- lib/src/http/endpoint.dart
+++ lib/src/http/endpoint.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/foundation.dart';
 
-String get uriPreffix => kReleaseMode
-    ? ''
-    : kProfileMode
-        ? 'stg.'
-        : kDebugMode
-            ? 'dev.'
-            : '';
+// String get uriPreffix => kReleaseMode
+//     ? ''
+//     : kProfileMode
+//         ? 'stg.'
+//         : kDebugMode
+//             ? 'dev.'
+//             : '';
 
-
-const prod = "https://api.dreampuppy.com.br";
 const uvicorn = "http://localhost:9900";
+
+/// Docker compose
 const compose = "http://localhost:8080";
 
-String get baseUrl => kDebugMode ? uvicorn :  prod;
+/// Local cloud (Minikube with port-forward)
+const cloud = "http://localhost:28015";
+
+const prod = "https://api.dreampuppy.com.br";
+
+ String get devEnv => cloud;
+
+String get baseUrl => kDebugMode ? devEnv : prod;
