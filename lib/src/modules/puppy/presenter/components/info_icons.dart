b70b6753eb52e6adc66398ebdf0116e5da2f023b
Marcelo Viana <marcelofv12@hotmail.com>
2024-03-16 04:07:59 -0300

    release 1.1.0

--- lib/src/modules/puppy/presenter/components/info_icons.dart
+++ lib/src/modules/puppy/presenter/components/info_icons.dart
@@ -36,262 +36,292 @@ class InfoIconsComponent extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 18.0),
       child: Column(
         children: [
-          InfoWithIconWidget(
-            icon: Tooltip(
-              preferBelow: false,
-              message: 'Raça do filhote',
-              child: Icon(
-                Icons.pets,
-                color: Colors.brown.shade400,
-              ),
-            ),
-            child: RichText(
-              overflow: TextOverflow.ellipsis,
-              text: TextSpan(
-                  text: 'Raça: ',
-                  style: DefaultTextStyle.of(
-                    context,
-                  ).style.copyWith(
-                        fontSize: fontSize,
-                      ),
-                  children: [
-                    TextSpan(
-                      text: puppyDetails.breed,
-                      style: DefaultTextStyle.of(
-                        context,
-                      ).style.copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                    const TextSpan(text: '.'),
-                  ]),
-            ),
-          ),
+          plotBreedWidget(context),
           const Gap(4),
-          InfoWithIconWidget(
-            icon: Tooltip(
-              preferBelow: false,
-              message: 'Gênero do filhote',
-              child: gender.icon,
-            ),
-            child: RichText(
-              overflow: TextOverflow.ellipsis,
-              text: TextSpan(
-                  text: 'Gênero: ',
-                  style: DefaultTextStyle.of(
-                    context,
-                  ).style.copyWith(
-                        fontSize: fontSize,
-                      ),
-                  children: [
-                    TextSpan(
-                      text: gender.text,
-                      style: DefaultTextStyle.of(
-                        context,
-                      ).style.copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                    const TextSpan(text: '.'),
-                  ]),
-            ),
-          ),
+          plotGenderWidget(gender, context),
           const Gap(4),
-          InfoWithIconWidget(
-            icon: Tooltip(
-              preferBelow: false,
-              message: 'Data de nascimento',
-              child: Icon(
-                Icons.cake_rounded,
-                color: Colors.pink.shade100,
-              ),
-            ),
-            child: RichText(
-              overflow: TextOverflow.ellipsis,
-              text: TextSpan(
-                text: 'Nascimento: ',
-                style: DefaultTextStyle.of(
-                  context,
-                ).style.copyWith(
-                      fontSize: fontSize,
-                    ),
-                children: [
-                  TextSpan(
-                    text: birth,
-                    style: DefaultTextStyle.of(
-                      context,
-                    ).style.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
-                  ),
-                  const TextSpan(text: '.'),
-                ],
-              ),
-            ),
-          ),
+          plotWeightWidget(context, weight),
           const Gap(4),
-          InfoWithIconWidget(
-            icon: Tooltip(
-              preferBelow: false,
-              message: 'Histórico de vacinação',
-              child: SizedBox(
-                height: 24,
-                child: Assets.icons.drugMedecineSyringueIcon
-                    .image(height: 24, color: Colors.red.shade300),
-              ),
-            ),
-            child: RichText(
-              overflow: TextOverflow.ellipsis,
-              text: TextSpan(
-                text: 'Vacinas: ',
-                style: DefaultTextStyle.of(
-                  context,
-                ).style.copyWith(fontSize: fontSize),
-                children: [
-                  TextSpan(
-                    text: 'Em breve',
-                    style: DefaultTextStyle.of(
-                      context,
-                    ).style.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
-                  ),
-                  /*
-                  TextSpan(
-                    text: '${vaccines.amount}x ',
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  TextSpan(
-                    text: vaccines.brand,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  */
-                  const TextSpan(text: '.'),
-                ],
-              ),
-              // child: Text(formatVaccinationRecordUsecase(
-              //     pet.vaccineRecord)),
-            ),
-          ),
+          plotBirthWidget(context, birth),
           const Gap(4),
-          InfoWithIconWidget(
-            icon: Tooltip(
-              preferBelow: false,
-              message: 'Vermífugos aplicados',
-              child: SizedBox(
-                  height: 24,
-                  child: Icon(
-                    Icons.bug_report,
-                    color: Colors.green.shade300,
-                  )),
-            ),
-            child: RichText(
-              overflow: TextOverflow.ellipsis,
-              text: TextSpan(
-                text: 'Vermífugos: ',
-                style: DefaultTextStyle.of(
-                  context,
-                ).style.copyWith(fontSize: fontSize),
-                children: [
-                  TextSpan(
-                    text: 'Em breve',
-                    style: DefaultTextStyle.of(
-                      context,
-                    ).style.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
-                  ),
-                  /*
-                  TextSpan(
-                    text: '${vermifuges.amount}x ',
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  TextSpan(
-                    text: vermifuges.brand,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  */
-                  const TextSpan(text: '.'),
-                ],
-              ),
-              // child: Text(formatVaccinationRecordUsecase(
-              //     pet.vaccineRecord)),
-            ),
-          ),
+          plotVaccinesWidget(context),
           const Gap(4),
-          InfoWithIconWidget(
-            icon: Tooltip(
-              preferBelow: false,
-              message: 'Microchip',
-              child: SizedBox(
-                height: 24,
-                child: Assets.icons.cpu
-                    .image(height: 24, color: Colors.amber.shade300),
-              ),
-            ),
-            child: RichText(
-              overflow: TextOverflow.ellipsis,
-              text: TextSpan(
-                text: 'Microchip: ',
-                style: DefaultTextStyle.of(
-                  context,
-                ).style.copyWith(
-                      fontSize: fontSize,
-                    ),
-                children: [
-                  TextSpan(
-                    text: microchip,
-                    style: DefaultTextStyle.of(
-                      context,
-                    ).style.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
-                  ),
-                  const TextSpan(text: '.'),
-                ],
-              ),
-            ),
-          ),
+          plotVermifugesWidget(context),
           const Gap(4),
-          InfoWithIconWidget(
-            icon: Tooltip(
-              preferBelow: false,
-              message: 'Última pesagem',
-              child: SizedBox(
-                height: 24,
-                child: SvgPicture.asset(
-                  Assets.icons.weightIcon.toString(),
-                  colorFilter: const ColorFilter.mode(
-                    Colors.blueGrey,
-                    BlendMode.srcIn,
-                  ),
-                ),
-              ),
-            ),
-            child: RichText(
-              overflow: TextOverflow.ellipsis,
-              text: TextSpan(
-                text: 'Peso: ',
-                style: DefaultTextStyle.of(
-                  context,
-                ).style.copyWith(
-                      fontSize: fontSize,
-                    ),
-                children: [
-                  TextSpan(
-                    text: weight,
-                    style: DefaultTextStyle.of(
-                      context,
-                    ).style.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
-                  ),
-                  const TextSpan(text: '.'),
-                ],
-              ),
-            ),
-          ),
+          plotMicrochipWidget(context, microchip),
         ],
+      ),
+    );
+  }
+
+  InfoWithIconWidget plotVaccinesWidget(BuildContext context) {
+    return InfoWithIconWidget(
+      icon: Tooltip(
+        preferBelow: false,
+        message: 'Histórico de vacinação',
+        child: SizedBox(
+          height: 24,
+          child: Assets.icons.drugMedecineSyringueIcon
+              .image(height: 24, color: Colors.red.shade300),
+        ),
+      ),
+      child: RichText(
+        overflow: TextOverflow.ellipsis,
+        text: TextSpan(
+          text: 'Vacinas: ',
+          style: DefaultTextStyle.of(
+            context,
+          ).style.copyWith(fontSize: fontSize),
+          children: [
+            TextSpan(
+              text: 'Em breve',
+              style: DefaultTextStyle.of(
+                context,
+              ).style.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            /*
+                TextSpan(
+                  text: '${vaccines.amount}x ',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: vaccines.brand,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                */
+            const TextSpan(text: '.'),
+          ],
+        ),
+        // child: Text(formatVaccinationRecordUsecase(
+        //     pet.vaccineRecord)),
+      ),
+    );
+  }
+
+  InfoWithIconWidget plotBirthWidget(BuildContext context, String birth) {
+    return InfoWithIconWidget(
+      icon: Tooltip(
+        preferBelow: false,
+        message: 'Data de nascimento',
+        child: Icon(
+          Icons.cake_rounded,
+          color: Colors.pink.shade100,
+        ),
+      ),
+      child: RichText(
+        overflow: TextOverflow.ellipsis,
+        text: TextSpan(
+          text: 'Nascimento: ',
+          style: DefaultTextStyle.of(
+            context,
+          ).style.copyWith(
+                fontSize: fontSize,
+              ),
+          children: [
+            TextSpan(
+              text: birth,
+              style: DefaultTextStyle.of(
+                context,
+              ).style.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const TextSpan(text: '.'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  InfoWithIconWidget plotGenderWidget(
+      ({Icon icon, String text}) gender, BuildContext context) {
+    return InfoWithIconWidget(
+      icon: Tooltip(
+        preferBelow: false,
+        message: 'Gênero do filhote',
+        child: gender.icon,
+      ),
+      child: RichText(
+        overflow: TextOverflow.ellipsis,
+        text: TextSpan(
+            text: 'Gênero: ',
+            style: DefaultTextStyle.of(
+              context,
+            ).style.copyWith(
+                  fontSize: fontSize,
+                ),
+            children: [
+              TextSpan(
+                text: gender.text,
+                style: DefaultTextStyle.of(
+                  context,
+                ).style.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const TextSpan(text: '.'),
+            ]),
+      ),
+    );
+  }
+
+  InfoWithIconWidget plotBreedWidget(BuildContext context) {
+    return InfoWithIconWidget(
+      icon: Tooltip(
+        preferBelow: false,
+        message: 'Raça do filhote',
+        child: Icon(
+          Icons.pets,
+          color: Colors.brown.shade400,
+        ),
+      ),
+      child: RichText(
+        overflow: TextOverflow.ellipsis,
+        text: TextSpan(
+            text: 'Raça: ',
+            style: DefaultTextStyle.of(
+              context,
+            ).style.copyWith(
+                  fontSize: fontSize,
+                ),
+            children: [
+              TextSpan(
+                text: puppyDetails.breed,
+                style: DefaultTextStyle.of(
+                  context,
+                ).style.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const TextSpan(text: '.'),
+            ]),
+      ),
+    );
+  }
+
+  InfoWithIconWidget plotVermifugesWidget(BuildContext context) {
+    return InfoWithIconWidget(
+      icon: Tooltip(
+        preferBelow: false,
+        message: 'Vermífugos aplicados',
+        child: SizedBox(
+            height: 24,
+            child: Icon(
+              Icons.bug_report,
+              color: Colors.green.shade300,
+            )),
+      ),
+      child: RichText(
+        overflow: TextOverflow.ellipsis,
+        text: TextSpan(
+          text: 'Vermífugos: ',
+          style: DefaultTextStyle.of(
+            context,
+          ).style.copyWith(fontSize: fontSize),
+          children: [
+            TextSpan(
+              text: 'Em breve',
+              style: DefaultTextStyle.of(
+                context,
+              ).style.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            /*
+                TextSpan(
+                  text: '${vermifuges.amount}x ',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: vermifuges.brand,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                */
+            const TextSpan(text: '.'),
+          ],
+        ),
+        // child: Text(formatVaccinationRecordUsecase(
+        //     pet.vaccineRecord)),
+      ),
+    );
+  }
+
+  InfoWithIconWidget plotMicrochipWidget(
+      BuildContext context, String microchip) {
+    return InfoWithIconWidget(
+      icon: Tooltip(
+        preferBelow: false,
+        message: 'Microchip',
+        child: SizedBox(
+          height: 24,
+          child:
+              Assets.icons.cpu.image(height: 24, color: Colors.amber.shade300),
+        ),
+      ),
+      child: RichText(
+        overflow: TextOverflow.ellipsis,
+        text: TextSpan(
+          text: 'Microchip: ',
+          style: DefaultTextStyle.of(
+            context,
+          ).style.copyWith(
+                fontSize: fontSize,
+              ),
+          children: [
+            TextSpan(
+              text: microchip,
+              style: DefaultTextStyle.of(
+                context,
+              ).style.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const TextSpan(text: '.'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  InfoWithIconWidget plotWeightWidget(BuildContext context, String weight) {
+    return InfoWithIconWidget(
+      icon: Tooltip(
+        preferBelow: false,
+        message: 'Última pesagem',
+        child: SizedBox(
+          height: 24,
+          child: SvgPicture.asset(
+            Assets.icons.weightIcon.toString(),
+            colorFilter: const ColorFilter.mode(
+              Colors.blueGrey,
+              BlendMode.srcIn,
+            ),
+          ),
+        ),
+      ),
+      child: RichText(
+        overflow: TextOverflow.ellipsis,
+        text: TextSpan(
+          text: 'Peso: ',
+          style: DefaultTextStyle.of(
+            context,
+          ).style.copyWith(
+                fontSize: fontSize,
+              ),
+          children: [
+            TextSpan(
+              text: weight,
+              style: DefaultTextStyle.of(
+                context,
+              ).style.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const TextSpan(text: '.'),
+          ],
+        ),
       ),
     );
   }
@@ -315,7 +345,7 @@ class InfoWithIconWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Flexible(flex: 1, child: icon),
-        Gap(18),
+        const Gap(18),
         Flexible(flex: 4, child: child),
       ],
     );
